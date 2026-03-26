733cdc09e43f4d604b857a5d6824165960726797
SaadaBrahim <saada.brahimbryan.studente@itispaleocapa.it>
2026-03-26 08:29:40 +0000

    completata parte 1

--- FestivalManager.java
+++ FestivalManager.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Iterator;
 public class FestivalManager{
     ArrayList<Guest> listaCantanti;
 
@@ -7,7 +7,28 @@ public FestivalManager(Guest primoG){
     this.listaCantanti = new ArrayList<>();
     this.listaCantanti.add(primoG);
 }
-    public void aggiungiCantante(Guest g) {
-        this.listaCantanti.add(g);
+public void aggiungiCantante(Guest g) {
+    this.listaCantanti.add(g);
+}
+
+    public void elimina(int indice) {
+        if (indice >= 0 && indice < listaCantanti.size()) {
+            listaCantanti.remove(indice);
+        }
     }
+
+    public void stampaLista() {
+        Iterator<Guest> it = this.listaCantanti.iterator();
+        while (it.hasNext()) {
+            Guest g = it.next();
+            System.out.println("Nome: " + g.nome + ", Età: " + g.eta);
+        }
+    }
+
+    public void sostituisci(int indice, Guest nuovoGuest) {
+        if (indice >= 0 && indice < listaCantanti.size()) {
+            listaCantanti.set(indice, nuovoGuest);
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -4,23 +4,27 @@ import java.util.Iterator;
 public class Main{
     public static void main(String[] args) {
         FestivalManager manager = new FestivalManager(new Guest("leo", 58));
-        manager.aggiungiCantante(new Guest("Elisa", 45));
+        manager.aggiungiCantante(new Guest("elisa", 45));
         
         Iterator<Guest> it = manager.listaCantanti.iterator();
         System.out.println("lista cantanti prima stampa 2 pp");
-        while (it.hasNext()) {
-            Guest g = it.next();
-            System.out.println("nome: " + g.nome + ", eta: " + g.eta);
-            }
+        manager.stampaLista();
         
+
         manager.aggiungiCantante(new Guest("GIGI", 87));
         manager.aggiungiCantante(new Guest("PEPE", 89));
 
         System.out.println("lista cantanti seconda stampa 4 pp");
-        while (it.hasNext()) {
-                Guest g = it.next();
-                System.out.println("nome: " + g.nome + ", eta: " + g.eta);
-            }
-        
+        manager.stampaLista();
+
+        manager.sostituisci(1, new Guest("annalisa", 38));
+        System.out.println("lista dopo sostituzione (annalisa al posto di elisa)");
+        manager.stampaLista();
+
+        manager.elimina(3);
+        System.out.println("lista cantanti selimino terzo (pepe)");
+
+        manager.stampaLista(); // no pepe
+
     }
 }
