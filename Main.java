a746f95a6b8b09f7b47d12ae2b45c5bd2fba55cc
SaadaBrahim <saada.brahimbryan.studente@itispaleocapa.it>
2026-03-26 07:55:59 +0000

    per prossima volta creare metodo in festival con cui stamp la lista con while e sistemare lista, e creazione metodo sostituisco dalla lista e riordino mettendo nuovo guest ultimo e sostituisco stessa posizione

--- Main.java
+++ Main.java
@@ -7,10 +7,20 @@ public class Main{
         manager.aggiungiCantante(new Guest("Elisa", 45));
         
         Iterator<Guest> it = manager.listaCantanti.iterator();
-        System.out.println("lista cantanti");
+        System.out.println("lista cantanti prima stampa 2 pp");
         while (it.hasNext()) {
             Guest g = it.next();
             System.out.println("nome: " + g.nome + ", eta: " + g.eta);
             }
+        
+        manager.aggiungiCantante(new Guest("GIGI", 87));
+        manager.aggiungiCantante(new Guest("PEPE", 89));
+
+        System.out.println("lista cantanti seconda stampa 4 pp");
+        while (it.hasNext()) {
+                Guest g = it.next();
+                System.out.println("nome: " + g.nome + ", eta: " + g.eta);
+            }
+        
     }
 }
